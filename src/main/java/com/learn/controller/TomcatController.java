688441da8ed6881998e12e6b1f74f95c5a7a4ba6
qin-gs <3210968849@qq.com>
2021-10-28 22:48:42 +0800

    转发

--- src/main/java/com/learn/controller/TomcatController.java
+++ src/main/java/com/learn/controller/TomcatController.java
@@ -1,8 +1,12 @@
 package com.learn.controller;
 
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * 访问路径 http://localhost:8090/tomcat/tomcat-test
@@ -15,5 +19,22 @@ public class TomcatController {
     public String tomcatTest() {
         System.out.println("tomcat test");
         return "tomcat test success";
+    }
+
+    @GetMapping("redirect")
+    public String redirect() {
+        return "redirect:/destination";
+    }
+
+    @GetMapping("destination")
+    @ResponseBody
+    public String destination() {
+        return "destination";
+    }
+
+    @ResponseBody
+    @GetMapping(value = "getMap", produces = MediaType.APPLICATION_JSON_VALUE)
+    public Object getMap() {
+        return new HashMap<>(Map.of("a", 1, "b", 2));
     }
 }
